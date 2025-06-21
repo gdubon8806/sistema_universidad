a751b918d52ce3242edcc023ff8f286b7887646d
Eduardo Jared Trujillo Gamero <167134639+JaredTrujillo01@users.noreply.github.com>
2025-06-20 23:05:41 -0600

    algunos cambios

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,13 @@
-document.getElementById('mostrarContrasena').addEventListener('change', function () {
-    const contrasenaInput = document.getElementById('contrasena');
-    contrasenaInput.type = this.checked ? 'text' : 'password';
-});
+const mostrarContrasena = document.getElementById('mostrarContrasena');
+if (mostrarContrasena) {
+    mostrarContrasena.addEventListener('change', function () {
+        const contrasenaInput = document.getElementById('contrasena');
+        if (contrasenaInput) {
+            contrasenaInput.type = this.checked ? 'text' : 'password';
+        }
+        const confirm = document.getElementById('confirmarContrasena');
+        if (confirm) {
+            confirm.type = this.checked ? 'text' : 'password';
+        }
+    });
+}
